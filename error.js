1c9d0fe4fba12ee4b3f7d434725b51aba1fb3a13
Sanmit0804 <sanmitsuthar3@gmail.com>
2024-12-30 17:17:24 +0530

    node error

--- error.js
+++ error.js
@@ -2,4 +2,14 @@
 
 const error = new Error("Something went wrong..!");
 // console.log(error);
-throw new Error("I am error object");
+// throw new Error("I am error object");
+
+const {CustomError} = require('./CustomError')
+throw new CustomError("This is our custom error ");
+
+
+// handle exception using try and catch
+
+try{
+    doSomething();
+}
