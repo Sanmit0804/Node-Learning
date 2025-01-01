c296df7b2e8a78fac0fdb89265047e83e46d4955
Sanmit0804 <sanmitsuthar3@gmail.com>
2025-01-01 21:17:41 +0530

    promise node js

---
+++ promise.js
@@ -0,0 +1,20 @@
+const promise = new Promise((resolve, reject) => {
+    console.log("Async task execution....")
+    throw "error re baba";
+    if (true) {
+        const person = {
+            name: "Sanmit"
+        }
+        resolve(person);
+    } else {
+        const error = {
+            errCode: "1001"
+        }
+        reject(error);
+    }
+})
+
+promise.then(
+    (val) => { console.log(val) },
+    (err) => { console.log(err) }
+).catch(() => console.log("Failed..!")).finally(() => console.log("cleanup"))
